7ba3572e374372b5cb4742663599268ede1cec22
rosus89 <rosus89@gmail.com>
2022-11-04 17:52:24 +0000

    X

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import inquirer from "inquirer";
+import fs from "fs";
+
 import Cat from "./models/Cat.js";
 import Dog from "./models/Dog.js";
+import Rabbit from "./models/Rabbit.js";
 import petType from "./choices/petType.js";
 import actions from "./choices/actions.js";
+import extras from "./choices/extras.js";
 import closeApp from "./choices/closeApp.js";
 
-import inquirer from "inquirer";
+
 
 
 const _ = inquirer.prompt;
+const separator = new inquirer.Separator();
 let pet;
+let savedPets = ["New Pet"];
 
 let $ = {
     init: async () => {
-         await $.createPet()
+         $.checkPets() == true ? await $.petLoadOptions() : await $.createPet();
          await $.setPetAct();
     },
     createPet: async () => {  await $.setPetType();
@@ -30,6 +37,9 @@ let $ = {
                                 case 'Dog':
                                     pet = new Dog();
                                     break;
+                                case 'Rabbit':
+                                    pet = new Rabbit();
+                                    break;
                             }
                         } )
                         },
@@ -39,10 +49,10 @@ let $ = {
                         .then((ans)=> pet.name = ans.name)
                         },
 
-    setPetAct: async () => { await _({ type:'list', name:'act',
+    setPetAct: async () => { await _({ type:'list', name:'activity',
                         message:'Select and activity to do with ' + pet.name,
-                        choices: [...actions.animal ,...actions[pet.constructor.name]]})
-                        .then((ans)=> pet[ans.act]())
+                        choices: [...actions.animal ,...actions[pet.constructor.name], separator , ...extras]})
+                        .then((ans)=> ans.activity=="Close" ? $.close() : pet[ans.activity]())
                         pet.stats()
                         $.setPetAct()
 
@@ -51,9 +61,44 @@ let $ = {
                         message:'Have you finished playing with your pet?',
                         choices: closeApp })
                     .then((ans)=>{
-                        console.log(ans)
+                        if (ans.close == "yes"){
+                            process.exit()
+                        }
+                        else if (ans.close == "saveExit"){
+                            $.save()
+                            process.exit()
+                        }
+                        
                     })
+                },
+    save: () => {
+                   !fs.existsSync("pets") ? fs.mkdirSync("pets") : null
+                    fs.appendFileSync("./pets/" + pet.name, JSON.stringify(pet), (err) => console.log(err))
+                },
+    checkPets: () => 
+                {   
+                    if(fs.existsSync("pets") === true){       
+                      fs.readdirSync("pets").forEach(file => savedPets.push(file))
+                      return true
                 }
+                    else return false
+                },
+    petLoadOptions: async () =>
+                {
+                    await _({   type:'list', name:'selectPet',
+                    message:'Load existing or create new pet',
+                    choices: savedPets  })
+                    .then((ans)=>{
+                        console.log(ans)
+                    }
+
+                    )
+                }
+    
+    // loadPet: async () => {
+    //                 fs.existsSync("pets") ?
+
+    // }
 }
 
 $.init();
